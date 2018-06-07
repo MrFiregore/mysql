9993d7748281a4adc37b2ca38b6eb58c69305cd4
Firegore <firegore2@gmail.com>
2018-06-07 20:04:20 +0200

    Library starts

--- src/Start.php
+++ src/Start.php
@@ -1,5 +1,9 @@
 <?php
     define("__ROOT__", substr(__DIR__, 0, strpos(__DIR__, "vendor") ?: strpos(__DIR__, "src")));
+    d(__ROOT__ . ".env");
+    if (!file_exists(__ROOT__ . ".env")) {
+        file_put_contents(__ROOT__ . ".env", "host=localhost\nuser=root\npassword=\ndatabase=\nport=3306");
+    }
     $dotenv = new Dotenv\Dotenv(__ROOT__);
     $dotenv->load();
     foreach ($_ENV as $key => $value) {
